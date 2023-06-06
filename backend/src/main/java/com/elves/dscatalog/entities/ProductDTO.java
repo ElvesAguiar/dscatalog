9a24b421e4fc0944299fa9008ee8ff4d58d18799
Elves Aguiar <elvesmhs@hotmail.com>
2023-06-06 09:00:58 -0300

    feat: ProductDTO

---
+++ backend/src/main/java/com/elves/dscatalog/entities/ProductDTO.java
@@ -0,0 +1,29 @@
+package com.elves.dscatalog.entities;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO() {
+
+    }
+
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        name = entity.getName();
+        description = entity.getDescription();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+}
