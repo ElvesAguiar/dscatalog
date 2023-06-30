4ce92ed566963ce7270a78fa3824b5637463ea0b
Elves Aguiar <elvesmhs@hotmail.com>
2023-06-30 17:09:14 -0300

    fix: categories problem

--- backend/src/main/java/com/elves/dscatalog/model/Category.java
+++ backend/src/main/java/com/elves/dscatalog/model/Category.java
@@ -1,17 +1,12 @@
 package com.elves.dscatalog.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "tb_category")
 public class Category {
     @Id
@@ -21,6 +16,47 @@ public class Category {
 
     @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
+
+
+    public Category(){
+
+    }
+
+    public Category(Long id, String name, Set<Product> products) {
+        this.id = id;
+        this.name = name;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Set<Product> getProducts() {
+        return products;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category category)) return false;
+        return Objects.equals(getId(), category.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 
 
 }
