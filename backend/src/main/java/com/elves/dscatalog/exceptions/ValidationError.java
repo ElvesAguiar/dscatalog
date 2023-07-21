ce9a8ca8d69d3405890134a2b26cc466dc6f495f
Elves Aguiar <elvesmhs@hotmail.com>
2023-07-21 10:17:08 -0300

    fix: validation working again

---
+++ backend/src/main/java/com/elves/dscatalog/exceptions/ValidationError.java
@@ -0,0 +1,26 @@
+package com.elves.dscatalog.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+	
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+}
--- backend/src/main/java/com/elves/dscatalog/exceptions/handler/ControllerExceptionHandler.java
+++ backend/src/main/java/com/elves/dscatalog/exceptions/handler/ControllerExceptionHandler.java
@@ -2,22 +2,40 @@ package com.elves.dscatalog.exceptions.handler;
 
 import com.elves.dscatalog.exceptions.DomainException;
 import com.elves.dscatalog.exceptions.StandardError;
+import com.elves.dscatalog.exceptions.ValidationError;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
 
 @ControllerAdvice
-public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+public class ControllerExceptionHandler {
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<StandardError> resourceNotFound(DomainException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
         return ResponseEntity.status(status).body(err);
     }
 
