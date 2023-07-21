390a33c4213f1b883ad49b532fec92c7d0401b86
Elves Aguiar <elvesmhs@hotmail.com>
2023-07-21 10:16:24 -0300

    feat: returning entityDto for findAll Products

--- backend/src/main/java/com/elves/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/elves/dscatalog/dto/UserInsertDTO.java
@@ -1,11 +1,15 @@
 package com.elves.dscatalog.dto;
 
 import com.elves.dscatalog.services.validation.UserInsertValid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @UserInsertValid
 
 public class UserInsertDTO extends UserDTO {
 
+    @NotBlank(message = "Campo obrigatório")
+    @Size(min = 8, message = "Deve ter no mínimo 8 caracteres")
     private String password;
 
     UserInsertDTO() {
